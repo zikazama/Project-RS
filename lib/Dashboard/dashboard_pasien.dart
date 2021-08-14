e96b80e1e214365aefac069d9cb518beebfc06be
febriananur <febriananurokhman@gmail.com>
2021-08-14 23:42:42 +0700

    add menu, dan perlu ditambahkan informasi/post di bawah menu

--- lib/Dashboard/dashboard_pasien.dart
+++ lib/Dashboard/dashboard_pasien.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class DashboardPasien extends StatelessWidget {
+class DashboardPasien extends StatefulWidget {
   @override
+_DashboardPasien createState() => _DashboardPasien();
+}
+
+class _DashboardPasien extends State<DashboardPasien> {
+
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color(0xFFFFFFFF),
         body: Stack(children: <Widget>[
-          Column(children: <Widget>[
+          Stack(children: <Widget>[
             Positioned(
               child: Container(
                 height: 250,
@@ -52,13 +58,102 @@ class DashboardPasien extends StatelessWidget {
             ),
             SafeArea(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizonal: 20),
+                padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Column(
-                  children: <Widget>[Container()],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.270),
+                    ),
+                    Expanded(
+                      child: GridView.count(
+                        padding: EdgeInsets.all(15),
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 20,
+                        mainAxisSpacing: 20,
+                        children: <Widget>[
+                          CategoryCard(
+                            srcSvg: "assets/icons/registration.svg",
+                            title: "Pendaftaran\nonline",
+                          ),
+                          CategoryCard(
+                            srcSvg: "assets/icons/cs.svg",
+                            title: "Konsultasi\nOnline",
+                          ),
+                          CategoryCard(
+                            srcSvg: "assets/icons/homecare.svg",
+                            title: "Home Care",
+                          ),
+                          CategoryCard(
+                            srcSvg: "assets/icons/emergency.svg",
+                            title: "Emergency",
+                          ),
+                          CategoryCard(
+                            srcSvg: "assets/icons/profile.svg",
+                            title: "Profile Anda",
+                          ),
+                        ],
+                      ),
+                    ),   
+                   Positioned(
+                      child: Container(
+                        margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.300),
+                          child: Container(
+                              child: Text(
+                            "INFORMASI GERIATRI",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
+                          )))),
+                  ],
                 ),
               ),
             )
           ]),
         ]));
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String title;
+  final String srcSvg;
+  const CategoryCard({
+    Key key,
+    this.title, this.srcSvg,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      
+      child: Container( 
+        decoration: BoxDecoration(
+            color: const Color(0xFFF8F8F8), borderRadius: BorderRadius.circular(10),
+           ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: (){},
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: Column(
+                children: <Widget>[
+                  Spacer(),
+                  // SvgPicture.asset("svgSrc"),
+                  SvgPicture.asset(srcSvg,height: 30,),
+                  Spacer(),
+                  Text(
+                    title,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
