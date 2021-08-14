eaca9ddc38486df01447c84e8995b19ebfeb85a1
febriananur <febriananurokhman@gmail.com>
2021-08-14 20:53:40 +0700

    add icon & dashboard in progress

--- lib/Dashboard/dashboard_pasien.dart
+++ lib/Dashboard/dashboard_pasien.dart
@@ -4,73 +4,61 @@ class DashboardPasien extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFFFFFF),
-      body: Stack(children: <Widget>[
-        Column(
-          children: <Widget>[
+        backgroundColor: const Color(0xFFFFFFFF),
+        body: Stack(children: <Widget>[
+          Column(children: <Widget>[
             Positioned(
               child: Container(
-              height: 250,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                image: AssetImage('assets/images/wave_dashboard.png'),
-                fit: BoxFit.cover,
-              )),
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                      child: Container(
-                          margin: EdgeInsets.only(
-                          bottom: MediaQuery.of(context).size.height * 0.120),
-                          child: Center(
-                              child: Text(
-                            "Dashboard",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),
-                          )))),
-                          Positioned(
-                            
-                      child: Container(
-                        padding: EdgeInsets.only(left:30.0),
-                          margin: EdgeInsets.only(
-                          top: MediaQuery.of(context).size.height * 0.100),
-                          child: SafeArea(
-                              child: Text(
-                            "Selamat datang User",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(color: Colors.white,fontWeight: FontWeight.normal,fontSize: 15),
-                          ))))
-                ],
+                height: 250,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage('assets/images/wave_dashboard.png'),
+                  fit: BoxFit.cover,
+                )),
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                        child: Container(
+                            margin: EdgeInsets.only(
+                                bottom:
+                                    MediaQuery.of(context).size.height * 0.120),
+                            child: Center(
+                                child: Text(
+                              "Dashboard",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20),
+                            )))),
+                    Positioned(
+                        child: Container(
+                            padding: EdgeInsets.only(left: 30.0),
+                            margin: EdgeInsets.only(
+                                top:
+                                    MediaQuery.of(context).size.height * 0.100),
+                            child: SafeArea(
+                                child: Text(
+                              "Selamat datang User",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 15),
+                            ))))
+                  ],
+                ),
               ),
             ),
-    
-        ),
-       
-        Expanded(
-          child: GridView.count(
-            crossAxisCount: 3,
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.black,
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizonal: 20),
+                child: Column(
+                  children: <Widget>[Container()],
                 ),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                ),
-              )
-            ],
-          ),
-        )
-      ]),
-      ]
-      )
-    );
+            )
+          ]),
+        ]));
   }
 }
