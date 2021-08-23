da0851be3909fbbb9da9b2c9e0c56e7bd27e29ac
CagionSource <78655390+CagionSource@users.noreply.github.com>
2021-08-24 00:53:31 +0700

    Delete lib/services directory

--- lib/services/services.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:convert';
-
-import 'package:aplikasi_rs/login_pasien.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
-
-part 'database.dart';
